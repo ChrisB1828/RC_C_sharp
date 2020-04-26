6f28e4c48ca17f3ab49676db00303bac195ea34f
ChrisB1828 <64245217+ChrisB1828@users.noreply.github.com>
2020-04-26 19:02:02 +0300

    Ielāde

---
+++ Day3_MD/Day3_MD/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Day3_MD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ievade();
+        }
+        
+        static void Ievade ()
+        {
+            int i = 1;
+            int lielākais = 0;
+            int mazākais = 0;
+
+            while (i > 0)
+            {
+                String stop = "";
+
+                for (int j = 1; j > 0;)
+                {
+                    Console.WriteLine("Ievadiet skaitli! Ja ievadīsiet STOP, skaitļu ievade tiks pārtraukta.");
+                    stop = Console.ReadLine();
+                }
+                 
+                if (stop == "STOP")
+                {
+                    break;
+                }
+
+                int sk = Convert.ToInt32(stop);
+
+                if (sk < mazākais)
+                {
+                    mazākais = sk;
+                }
+                else if (sk >= lielākais)
+                {
+                    lielākais = sk;
+                }           
+            }
+
+            Console.WriteLine("Lielākais ievadītais skaitlis ir " + lielākais);
+            Console.WriteLine("Mazākais ievadītais skailis ir  " + mazākais);
+        }
+    }
+}
---
+++ Day3_loops/Day3_loops/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Day3_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uzdevums4();
+        }
+
+        static void Uzdevums4()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");             
+            }
+            for (int i = 2; i >= 0; i--) //īsti nesaptoru, kāpec šādi sanāk, lai gan es definēju ka loop ir jātaisa tikai tad, ja j < i un ja es definēju, ka j <= i tad nesanāk.
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+
+        }
+        static void Uzdevums3()
+        {
+           
+            String combined = "";
+            for (int i = 0; i < 4; i++)
+            {
+                combined = combined + "*";
+                Console.WriteLine(combined);
+            }
+
+        }
+        static void Uzdevums3v2()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Uzdevums2()
+        {
+            int positive = 0;
+            int negative = 0;
+            int nule = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ievadi skaitli!");
+                int sk = Convert.ToInt32(Console.ReadLine());
+                if (sk > 0)
+                {
+                    positive += 1; // var izmantot arī positive++ ja ir jāpieskaita vērtība 1, += var pieskaitīt arī citas vērtības ne tikai 1, bet piemēram arī 5,31, 141 utt
+                }
+                else if (sk == 0)
+                {
+                    nule += 1;
+                }
+                else 
+                {
+                    negative += 1;
+                }
+
+            }
+
+            Console.WriteLine("Ievadīti " + positive + " positīvi skaitļi");
+            Console.WriteLine("Ievadīti " + negative + " negatīvi skaitļi");
+            Console.WriteLine("Ievadīti " + nule + " skaitļi ar vērtību 0");
+        }
+        static void Uzdevums()
+        {
+            int lielakais = 0;
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ievadi skaitli!");
+                 int sk = Convert.ToInt32(Console.ReadLine());
+                if (sk >= lielakais)
+                {
+                    lielakais = sk;
+                }
+            
+            }
+            
+                Console.WriteLine("Lielākais skaitlis no ievadītajiem ir " + lielakais);
+            
+        }
+        static void ForExample()
+        {
+            for (int i = 0; i < 10; i++) //izmantojam, kad ir zināms reižu skaits cik garšs cikls ir jātaisa
+            // i++ , i = i + 1
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(1);
+                Console.WriteLine("-------");
+            }
+
+            Console.WriteLine("Ejam atpakaļ.");
+            Console.WriteLine("-------");
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(1);
+                Console.WriteLine("-------");
+            }
+        }
+    }
+}
