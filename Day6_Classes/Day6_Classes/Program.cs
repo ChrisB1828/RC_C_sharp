56ac4df7b9658075d1d0ab7d95a19cabc2ff1148
ChrisB1828 <64245217+ChrisB1828@users.noreply.github.com>
2020-05-06 18:50:36 +0300

    ielade

--- Day6_Classes/Day6_Classes/Program.cs
+++ Day6_Classes/Day6_Classes/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Dynamic;
+using System.Threading.Tasks;
 
 namespace Day6_Classes
 {
@@ -6,12 +8,31 @@ namespace Day6_Classes
     {
         static void Main(string[] args)
         {
+
+            Task22();
+
+        }
+
+        static void Task22()
+        {
+            Console.WriteLine("Ievadiet pirmo skaitli!");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ievadiet otro skaitli!");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+
+            Task1.setA(sk1);
+            Task1.setB(sk2);
+
+            Console.WriteLine(Task1.getA() + Task1.getB());
+        }
+
+        static void Task1_1()
+        {
             Console.WriteLine("Ievadiet skaitli! Skaiitlis norādīs uz rindu skaitu, cik rindās tiks izvadītas zvaigznītes");
             int a = Convert.ToInt32(Console.ReadLine());
 
             Task1.Zvaigznes(a);
-
-
         }
     }
 }
--- Day6_Classes/Day6_Classes/Task1.cs
+++ Day6_Classes/Day6_Classes/Task1.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
+using System.Threading;
 
 namespace Day6_Classes
 {
     class Task1
     {
+        private static int a = 0;
+        private static int b = 0;
+
+        public static void setA(int localA)
+        {
+            a = localA;
+        }
+
+        public static void setB(int localB)
+        {
+            b = localB;
+        }
+
+        public static int getA()
+        {
+            return a;
+        }
+        public static int getB()
+        {
+            return b;
+        }
 
         public static void Zvaigznes(int a)
         {
