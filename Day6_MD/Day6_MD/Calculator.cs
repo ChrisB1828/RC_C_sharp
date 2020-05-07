b3827f0a1852f4a83fdbb28f444261d5d565ba5e
ChrisB1828 <64245217+ChrisB1828@users.noreply.github.com>
2020-05-07 12:09:54 +0300

    MD ielade

---
+++ Day6_MD/Day6_MD/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Day6_MD
+{
+    class Calculator
+    {
+
+        public static int Addition(int a, int b)
+        {
+            int sumAdd = a + b;
+            return sumAdd;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            int sumSub = a - b;
+            return sumSub;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            int sumMulti = a * b;
+            return sumMulti;
+        }
+
+        public static int Division(int a, int b)
+        {
+            int sumDiv = a / b;
+            return sumDiv;
+        }
+
+        public static int Reminder(int a, int b)
+        {
+            int reminder = a % b;
+            return reminder;
+        }
+
+    }
+}
---
+++ Day6_MD/Day6_MD/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Day6_MD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator();
+        }
+                
+        static void Calculator()
+        {
+            Console.WriteLine("Ja vēlies saskaitīt divus skaitļus tad nospied - 1");
+            Console.WriteLine("Ja vēlies atņemt divus skaiļus - 2");
+            Console.WriteLine("Ja vēlies reizināt divus skaitļus tad nospied - 3");
+            Console.WriteLine("Ja vēlies dalīt divus skaitļus tad nospied - 4");
+            Console.WriteLine("Ja vēlies iegūt atlikumu no skaitļa tad nospied - 5");
+            Console.WriteLine("Ja velies noteikt skaitli 'n' pakāpē tad nospied - 6");
+            Console.WriteLine("Ja velies pārtraukt kalkulatora darbību, tad raksti STOP");
+            Console.WriteLine();
+
+            string izvele = Console.ReadLine();
+            Console.WriteLine();
+
+            izvele = izvele.ToLower();
+
+            
+            if (izvele == "stop")
+            {
+                Console.WriteLine("Paldies par uzmanību!");
+            }
+            else if (izvele == "1")
+            {
+
+            }
+            else if (izvele == "2")
+            {
+
+            }
+            else if (izvele == "3")
+            {
+
+            }
+            else if (izvele == "4")
+            {
+
+            }
+            else if (izvele == "5")
+            {
+
+            }
+            else if (izvele == "6")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Nepareiza ievade!");
+                Console.WriteLine("Ievadiet savu izvēli atkārtoti!");
+                Console.WriteLine();
+
+                Calculator();
+            }
+
+        }
+
+    }
+}
