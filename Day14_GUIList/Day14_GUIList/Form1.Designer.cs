3beb258e13f59f43a05a2a82128edcaff9f231c4
Kristaps Balodis <64245217+ChrisB1828@users.noreply.github.com>
2020-05-27 17:58:22 +0300

    klases darbs ielāde

---
+++ Day14_GUIList/Day14_GUIList/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Day14_GUIList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LstTest = new System.Windows.Forms.ListView();
+            this.BtnTest = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnChange = new System.Windows.Forms.Button();
+            this.FieldInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LstTest
+            // 
+            this.LstTest.HideSelection = false;
+            this.LstTest.Location = new System.Drawing.Point(12, 12);
+            this.LstTest.Name = "LstTest";
+            this.LstTest.Size = new System.Drawing.Size(208, 339);
+            this.LstTest.TabIndex = 0;
+            this.LstTest.UseCompatibleStateImageBehavior = false;
+            this.LstTest.View = System.Windows.Forms.View.List;
+            // 
+            // BtnTest
+            // 
+            this.BtnTest.Location = new System.Drawing.Point(250, 108);
+            this.BtnTest.Name = "BtnTest";
+            this.BtnTest.Size = new System.Drawing.Size(75, 23);
+            this.BtnTest.TabIndex = 1;
+            this.BtnTest.Text = "ADD";
+            this.BtnTest.UseVisualStyleBackColor = true;
+            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(250, 137);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 3;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnChange
+            // 
+            this.BtnChange.Location = new System.Drawing.Point(250, 167);
+            this.BtnChange.Name = "BtnChange";
+            this.BtnChange.Size = new System.Drawing.Size(75, 23);
+            this.BtnChange.TabIndex = 4;
+            this.BtnChange.Text = "Change Text";
+            this.BtnChange.UseVisualStyleBackColor = true;
+            this.BtnChange.Click += new System.EventHandler(this.BtnChange_Click);
+            // 
+            // FieldInput
+            // 
+            this.FieldInput.Location = new System.Drawing.Point(250, 82);
+            this.FieldInput.Name = "FieldInput";
+            this.FieldInput.Size = new System.Drawing.Size(100, 20);
+            this.FieldInput.TabIndex = 5;
+            this.FieldInput.TextChanged += new System.EventHandler(this.FieldInput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FieldInput);
+            this.Controls.Add(this.BtnChange);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnTest);
+            this.Controls.Add(this.LstTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView LstTest;
+        private System.Windows.Forms.Button BtnTest;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnChange;
+        private System.Windows.Forms.TextBox FieldInput;
+    }
+}
+
---
+++ Day14_GUIList/Day14_GUIList/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Day14_GUIList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(FieldInput.Text))
+            {
+                //nekas nenotiek
+            }
+            else
+            {
+                LstTest.Items.Add(FieldInput.Text);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) // button delete
+        {
+            foreach (ListViewItem item in LstTest.SelectedItems)
+            {
+                LstTest.Items.Remove(item);
+            }
+        }
+
+        private void FieldInput_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnChange_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in LstTest.SelectedItems)
+            {
+                item.Text = FieldInput.Text;
+            }
+        }
+    }
+}
