0f93b6fd132e5bf5467e1d976e2d5b98d85ce061
Kristaps Balodis <64245217+ChrisB1828@users.noreply.github.com>
2020-05-31 20:04:17 +0300

    klases darbs 3

---
+++ Day15_FileIO_CW3/Day15_FileIO_CW3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Day15_FileIO_CW3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BoxForText = new System.Windows.Forms.RichTextBox();
+            this.BtnText1 = new System.Windows.Forms.Button();
+            this.BtnText2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BoxForText
+            // 
+            this.BoxForText.Location = new System.Drawing.Point(12, 12);
+            this.BoxForText.Name = "BoxForText";
+            this.BoxForText.Size = new System.Drawing.Size(180, 426);
+            this.BoxForText.TabIndex = 0;
+            this.BoxForText.Text = "";
+            // 
+            // BtnText1
+            // 
+            this.BtnText1.Location = new System.Drawing.Point(198, 28);
+            this.BtnText1.Name = "BtnText1";
+            this.BtnText1.Size = new System.Drawing.Size(75, 23);
+            this.BtnText1.TabIndex = 1;
+            this.BtnText1.Text = "Add Text 1";
+            this.BtnText1.UseVisualStyleBackColor = true;
+            this.BtnText1.Click += new System.EventHandler(this.BtnText1_Click);
+            // 
+            // BtnText2
+            // 
+            this.BtnText2.Location = new System.Drawing.Point(199, 58);
+            this.BtnText2.Name = "BtnText2";
+            this.BtnText2.Size = new System.Drawing.Size(75, 23);
+            this.BtnText2.TabIndex = 2;
+            this.BtnText2.Text = "Add Text 2";
+            this.BtnText2.UseVisualStyleBackColor = true;
+            this.BtnText2.Click += new System.EventHandler(this.BtnText2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnText2);
+            this.Controls.Add(this.BtnText1);
+            this.Controls.Add(this.BoxForText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox BoxForText;
+        private System.Windows.Forms.Button BtnText1;
+        private System.Windows.Forms.Button BtnText2;
+    }
+}
+
---
+++ Day15_FileIO_CW3/Day15_FileIO_CW3/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day15_FileIO_CW3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void Read(String fileName, List<String> lst)
+        {
+            String line;
+            try
+            {
+                
+                StreamReader sr = new StreamReader($@"D:\Riga Coding School\Day15_FileIO\ClassWork2\{fileName}.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    lst.Add(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Nav atrasts fails!");
+            }
+        }
+
+        private void BtnText1_Click(object sender, EventArgs e)
+        {
+            List<string> lst = new List<string>();
+            Read("Test", lst);
+
+            BoxForText.Text = File.ReadAllText($@"D:\Riga Coding School\Day15_FileIO\ClassWork2\Test.txt");
+
+        }
+
+        private void BtnText2_Click(object sender, EventArgs e)
+        {
+            List<string> lst = new List<string>();
+            Read("Test_new", lst);
+
+            BoxForText.Text = File.ReadAllText($@"D:\Riga Coding School\Day15_FileIO\ClassWork2\Test_new.txt");
+        }
+    }
+}
