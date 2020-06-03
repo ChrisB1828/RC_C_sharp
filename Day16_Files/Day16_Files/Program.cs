0009c4d9c76dfee28cc8bbb4d1124cd6cb7137de
Kristaps Balodis <64245217+ChrisB1828@users.noreply.github.com>
2020-06-03 16:41:10 +0300

    task1

---
+++ Day16_Files/Day16_Files/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day16_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.Run();
+        }
+        static void TaskPecTam()
+        {
+            String name = "";
+            String surname = "";
+            int course = 0;
+
+            Student lst = new Student(name, surname, course);
+            List<Student> listOfStudents = new List<Student>();
+
+
+            int i = 1;
+
+            while (i > 0)
+            {
+                Console.WriteLine("Lai izvadītu sarakstu ar studentiem nospied - 1");
+                Console.WriteLine("Lai pievienotu sarakstam studentu nospied - 2");
+                Console.WriteLine("Lai izietu no saraksta nospied - 0");
+                String ievade = Console.ReadLine();
+
+                switch (ievade)
+                {
+                    case "1":
+                        foreach (Student st in listOfStudents)
+                        {
+                            st.print();
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Ievadiet studenta vārdu:");
+                        name = Console.ReadLine();
+
+                        Console.WriteLine("Ievadiet studenta uzvārdu:");
+                        surname = Console.ReadLine();
+
+                        Console.WriteLine("Ievadiet studenta kursu:");
+                        course = Convert.ToInt32(Console.ReadLine());
+
+                        listOfStudents.Add(new Student(name, surname, course));
+                        break;
+
+                    case "0":
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Day16_Files/Day16_Files/Task1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Transactions;
+
+namespace Day16_Files
+{
+    class Task1
+    {
+        public static void Run()
+        {
+            String studentInfo = "Juris,Abolins,3";
+            String[] words = studentInfo.Split(',');
+
+            //sadalīt pa vērtībam un un tand 
+            //pievienot sarakstam;
+            string name = words[0];
+            string surname = words[1];
+            int course = Convert.ToInt32(words[2]);
+
+            Student student = new Student(name, surname, course);
+
+            student.print();
+
+        }
+    }
+}
