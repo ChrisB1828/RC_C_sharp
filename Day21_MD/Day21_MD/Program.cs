24cdf0bb532d7a7f317f644adc6ad1c88f42e339
Kristaps Balodis <64245217+ChrisB1828@users.noreply.github.com>
2020-06-20 12:41:29 +0300

    ielade

--- Day21_MD/Day21_MD/Program.cs
+++ Day21_MD/Day21_MD/Program.cs
@@ -1,4 +1,5 @@
-﻿using Renci.SshNet.Messages.Connection;
+﻿using Google.Protobuf.WellKnownTypes;
+using Renci.SshNet.Messages.Connection;
 using System;
 
 namespace Day21_MD
@@ -56,16 +57,72 @@ namespace Day21_MD
             Console.WriteLine();
             Console.WriteLine("----------------------------");
 
-            Console.WriteLine("Enter name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter last name:");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter Cours:");
-            int cours = Convert.ToInt32(Console.ReadLine());
+            string name = "";
+            string lastName = "";
+            int cours = 0;
 
-            University student = new University(name, lastName, cours);
-            
-            con.AddStudent(student);
+            bool b1;
+            bool b2;
+
+            Console.WriteLine("Enter name:");
+            name = Console.ReadLine();
+
+            if (b1 = string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Please enter students name!");
+                b1 = false;    
+            }
+            else
+            { 
+                b1 = true;
+            }
+
+            if (b1 == true)
+            {
+                Console.WriteLine("Enter last name:");
+                lastName = Console.ReadLine();
+
+                if (b2 = string.IsNullOrWhiteSpace(lastName))
+                {
+                    Console.WriteLine("Please enter students last name!");
+                    b2 = false;
+                }
+                else
+                {
+                    b2 = true;
+                }
+
+                if (b2 == true)
+                {
+                    Console.WriteLine("Enter Cours:");
+                    string coursString = Console.ReadLine();
+
+                    try
+                    {
+                        cours = Convert.ToInt32(coursString);
+
+                        University student = new University(name, lastName, cours);
+
+                        con.AddStudent(student);
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("Please enter student cours with numbers");
+                        AddStudent(con);
+                    }
+
+                    
+                }
+                else
+                {
+                    AddStudent(con);
+                }  
+            }
+            else
+            {
+                AddStudent(con);
+            }
 
             Console.WriteLine("----------------------------");
             Console.WriteLine();
